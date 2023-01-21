2e6f7e1a135787457e662a41f39839a5bb82008c
vinicius-guima <vinicius_guima.social@hotmail.com>
2023-01-21 12:09:06 -0300

    terceiro commit

--- src/main/java/maven/Teste.java
+++ src/main/java/maven/Teste.java
@@ -2,7 +2,10 @@ package maven;
 
 public class Teste {
 	public static void main(String[] args) {
+		System.err.println("----------");
 		System.err.println("test prepare and perform");
+		System.err.println("----------");
+
 	}
 
 }
